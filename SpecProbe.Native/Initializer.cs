66ab01acaed008f26912925c62840b38069efdad
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-12-10 12:53:16 +0300

    upd - Updated copyright year

--- SpecProbe.Native/Initializer.cs
+++ SpecProbe.Native/Initializer.cs
@@ -1,20 +1,21 @@
-﻿
-// SpecProbe  Copyright (C) 2020-2021  Aptivi
-// 
+﻿//
+// SpecProbe  Copyright (C) 2023-2024  Aptivi
+//
 // This file is part of SpecProbe
-// 
+//
 // SpecProbe is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // SpecProbe is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 using SpecProbe.Software.Kernel;
 using System;
