2af2d70b944d50b40a60c0999290744b42df82b8
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-10-27 11:00:55 +0300

    imp - Loop through resources for localization
    ---
    
    We need to loop through resources for localization.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/SpecProbe.Loader/Languages/LanguageTools.cs
+++ public/SpecProbe.Loader/Languages/LanguageTools.cs
@@ -17,16 +17,20 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+using ResourceLab.Management;
 using MainLangTools = SpecProbe.Software.Languages.LanguageTools;
 
 namespace SpecProbe.Loader.Languages
 {
     internal static class LanguageTools
     {
+        private const string LocalName = "SpecProbe.Loader";
+
         internal static string GetLocalized(string id)
         {
-            if (!MainLangTools.resourceManagers.ContainsKey("SpecProbe.Loader"))
-                MainLangTools.resourceManagers.Add("SpecProbe.Loader", new("SpecProbe.Loader.Resources.Languages.Output.Localizations", typeof(LanguageTools).Assembly));
+            // Add local resource
+            if (!ResourcesManager.ResourceManagerExists(LocalName))
+                ResourcesManager.AddResourceManager(LocalName, new($"{LocalName}.Resources.Languages.Output.Localizations", typeof(LanguageTools).Assembly));
             return MainLangTools.GetLocalized(id);
         }
     }
